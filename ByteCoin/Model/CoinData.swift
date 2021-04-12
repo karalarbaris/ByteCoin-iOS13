8c6318e2b58dcde06936fe7ecbd5c44045f65632
Baris <karalarbaris@gmail.com>
2021-04-12 17:34:19 +0200

    164. Step 1: Clone the Starting Project 165. Step 2: UIPickerView Functionality 167. Step 3: Make the API Request 168. Step 4: Parse the JSON Data

---
+++ ByteCoin/Model/CoinData.swift
@@ -0,0 +1,13 @@
+//
+//  CoinData.swift
+//  ByteCoin
+//
+//  Created by Baris Karalar on 12.04.2021.
+//  Copyright © 2021 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct CoinData: Codable {
+    let rate: Double
+}
--- ByteCoin/Model/CoinManager.swift
+++ ByteCoin/Model/CoinManager.swift
@@ -11,9 +11,44 @@ import Foundation
 struct CoinManager {
     
     let baseURL = "https://rest.coinapi.io/v1/exchangerate/BTC"
-    let apiKey = "YOUR_API_KEY_HERE"
+    let apiKey = "0AE5EB17-FC0B-4CC0-A847-4F90876FA1F8"
     
     let currencyArray = ["AUD", "BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","USD","ZAR"]
 
+    func getCoinPrice(for currency: String) {
+        let urlString = "\(baseURL)/\(currency)/?apikey=\(apiKey)"
+        performRequest(with: urlString)
+    }
+    
+    func performRequest(with urlString: String) {
+        if let url = URL(string: urlString) {
+            let session = URLSession(configuration: .default)
+            let task = session.dataTask(with: url) { (data, response, error) in
+                if error != nil {
+                    print(error)
+                    return
+                }
+                if let safeData = data {
+                    let dataString = String(data: safeData, encoding: .utf8)
+                    print(dataString)
+                    parseJSON(safeData)
+                }
+            }
+            task.resume()
+        }
+    }
+    
+    func parseJSON(_ coinData: Data) {
+        let decoder = JSONDecoder()
+        do {
+            let decodedData = try decoder.decode(CoinData.self, from: coinData)
+            let rate = decodedData.rate
+            print("rate is: \(rate)")
+        } catch  {
+            print(error )
+        }
+        
+        
+    }
     
 }
